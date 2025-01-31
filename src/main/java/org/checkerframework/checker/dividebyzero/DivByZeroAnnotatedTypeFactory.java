23c2131dd61110de57225e76ca6c1356b1c0d8d6
Jacqueline He <jacquelinehe@Jacquelines-MacBook-Pro-612.local>
2025-01-30 23:05:09 -0800

    step 6

--- src/main/java/org/checkerframework/checker/dividebyzero/DivByZeroAnnotatedTypeFactory.java
+++ src/main/java/org/checkerframework/checker/dividebyzero/DivByZeroAnnotatedTypeFactory.java
@@ -28,12 +28,22 @@ public class DivByZeroAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
     switch (literal.getKind()) {
       case INT_LITERAL:
         int intValue = (Integer) literal.getValue();
-        // TODO
-        break;
+        if (intValue > 0) {
+            return Positive.class;
+        } else if (intValue < 0) {
+            return Negative.class;
+        } else {
+            return Zero.class;
+        }
       case LONG_LITERAL:
         long longValue = (Long) literal.getValue();
-        // TODO
-        break;
+        if (longValue > 0) {
+            return Positive.class;
+        } else if (longValue < 0) {
+            return Negative.class;
+        } else {
+            return Zero.class;
+        }
     }
     return Top.class;
   }
